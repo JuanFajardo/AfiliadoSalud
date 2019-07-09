64437b144e2fe4ddc0f5810f700ab7575ce11f2a
Juan Fajardo <bett0@potosi.bo>
2019-07-09 15:26:26 -0400

    Modelo CentroSalud

--- app/Centro.php
+++ app/Centro.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Centro extends Model
 {
     protected $table = 'centros';
-    protected $fillable = ['id' , 'id_user', 'centro'];
+    protected $fillable = ['id' , 'centro'];
 }
